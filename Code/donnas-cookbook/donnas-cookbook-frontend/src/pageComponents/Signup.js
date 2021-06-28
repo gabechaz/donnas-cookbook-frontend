d632a10116b8f009ba848986befa07c61a7a1379
Gabriel Chazanov <gabe.chaz@gmail.com>
2021-06-28 16:45:04 -0400

    added signup form

--- Code/donnas-cookbook/donnas-cookbook-frontend/src/pageComponents/Signup.js
+++ Code/donnas-cookbook/donnas-cookbook-frontend/src/pageComponents/Signup.js
@@ -1,4 +1,22 @@
+import {useState} from 'react'
+
+const [name, setName] = useState("")
+
+const [nationality, setNationality] = useState("")
+
 function Signup () {
+    return (
+        <form>
+            <label>
+                Name
+                <input type='text'></input>
+            </label>
+            <label>
+                Nationality
+                <input type='text'></input>
+            </label>
+        </form>
+    )
 
 }
 
