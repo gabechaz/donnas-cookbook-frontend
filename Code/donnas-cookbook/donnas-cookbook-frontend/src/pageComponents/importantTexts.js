3b8a3803b63c780b07f364acb1c685445def6c38
Gabriel Chazanov <gabe.chaz@gmail.com>
2021-07-12 16:51:10 -0400

    added die result options

--- Code/donnas-cookbook/donnas-cookbook-frontend/src/pageComponents/importantTexts.js
+++ Code/donnas-cookbook/donnas-cookbook-frontend/src/pageComponents/importantTexts.js
@@ -7,6 +7,22 @@ finding a good boyfriend and making it to prom
 
 The board consists of a town that your pawn will be traversing over the course of the game by rolling dice 
 
-when you roll a dispatchEvent, you can either use the die result to advance or to take an action as proscirubed
-on the dice. So there are six AuthenticatorAssertionResponse, one for each die
+when you roll a die, you can either use the die result to advance or to take an action as proscirubed
+on the dice. So there are six actions, one for each die
 
+the six actions are as follows(subject to change)
+
+1. work on car(must have car)
+
+2. take a selfie
+
+3. work day job
+
+4. do homework
+
+5. exercise
+
+6. 
+
+
+hold on, this has got me thinking. What if the spots 
