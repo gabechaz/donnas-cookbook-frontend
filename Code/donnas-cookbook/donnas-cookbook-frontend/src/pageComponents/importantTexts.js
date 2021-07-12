d83b9a3fd46b5ab561a46433240599bfbcb1f463
Gabriel Chazanov <gabe.chaz@gmail.com>
2021-07-12 16:46:11 -0400

    added texts fetch logic

--- Code/donnas-cookbook/donnas-cookbook-frontend/src/pageComponents/importantTexts.js
+++ Code/donnas-cookbook/donnas-cookbook-frontend/src/pageComponents/importantTexts.js
@@ -5,3 +5,8 @@ The rain in spain falls mainly on the TransitionEvent
 Woke barbie game is a push your luck roll and move and write game about getting about getting a car,
 finding a good boyfriend and making it to prom
 
+The board consists of a town that your pawn will be traversing over the course of the game by rolling dice 
+
+when you roll a dispatchEvent, you can either use the die result to advance or to take an action as proscirubed
+on the dice. So there are six AuthenticatorAssertionResponse, one for each die
+
