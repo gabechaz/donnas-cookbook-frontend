1c258c41a6182088f9410c1af9bbbe11ed05f5ea
Gabriel Chazanov <gabe.chaz@gmail.com>
2021-09-27 17:17:12 -0400

    added classnames to recipe JSX elements

--- Code/donnas-cookbook/donnas-cookbook-frontend/src/pageComponents/Recipe.js
+++ Code/donnas-cookbook/donnas-cookbook-frontend/src/pageComponents/Recipe.js
@@ -65,23 +65,23 @@ function Recipe ({API}) {
 
 
     return (
-        <div>
+        <div className = 'outer-recipe-div'>
             {recipe ? 
                 <div>
-                    <h1>{recipe.name}</h1>
-                    <h3>Ingredients</h3>
-                    <ul>
+                    <h1 className = 'recipe-title'>{recipe.name}</h1>
+                    <h3 className = 'ingredients-subheading'>Ingredients</h3>
+                    <ul className='ingredients-ul'>
                         {ingredientLis}
                     </ul>
                     <br />
 
-                    <h3>Instructions</h3>
-                    <ul>
+                    <h3 className = 'instructions-subheading'>Instructions</h3>
+                    <ul className='instructions-li'>
                         {instructionLis}
                     </ul>
 
-                    <h3>Note</h3>
-                    <p>{recipe.note}</p>
+                    <h3 className='note-heading'>Note</h3>
+                    <p className='note'>{recipe.note}</p>
                 </div>
                 : 
                 <h1>Loading</h1>
